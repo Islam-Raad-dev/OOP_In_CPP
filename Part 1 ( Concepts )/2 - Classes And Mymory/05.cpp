502b0dc575f707a2ed58b156550c9ec63ba8b8a4
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-21 19:41:14 +0300

    Add

--- Part 1 ( Concepts )/2 - Classes And Mymory/05.cpp
+++ Part 1 ( Concepts )/2 - Classes And Mymory/05.cpp
@@ -2,5 +2,38 @@
 
 Access Specifiers (Modifiers):
 
+In C++, access specifiers (also known as access modifiers) are keywords that define the accessibility of class members (variables and functions). The three main access specifiers are:
 
 */
+#include <iostream> 
+using namespace std;
+
+class clsPerson 
+{
+
+private:
+    short Age;
+
+public:
+    string FirstName;
+    string LastName;
+
+    string FullName()
+    {
+        return FirstName + " " + LastName;
+    }
+
+};
+
+int main()
+{
+    clsPerson Person1;
+
+    Person1.FirstName = "Islam";
+    Person1.LastName = "Raad";
+
+    cout << Person1.FullName() << endl;
+
+
+    return 0;
+}
