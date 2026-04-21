d23519ff65166d8b27ba42d203779a5f33c0de33
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-21 19:52:01 +0300

    Add

--- Part 1 ( Concepts )/2 - Classes And Mymory/05.cpp
+++ Part 1 ( Concepts )/2 - Classes And Mymory/05.cpp
@@ -20,6 +20,7 @@ class clsPerson
 private:
 
     int Variabl1 = 5;
+
     int Function1()
     {
         return 40;
@@ -27,7 +28,8 @@ private:
 
 protected:
 
-    int Variable2 = 100;
+    int Variabl2 = 100;
+    
     int Function2()
     {
         return 50;
@@ -35,13 +37,32 @@ protected:
 
 public:
 
+    string FirstName;
+    string LastName;
 
+    string FullName()
+    {
+        return FirstName + " " + LastName;
+    }
+
+    float Function3()
+    {
+        return Function1() * Variabl1 * Variabl2;
+    }
+    
 
 };
 
 int main()
 {
     clsPerson Person1;
+
+    Person1.FirstName = "Islam";
+    Person1.LastName = "Raad";
+
+    cout << "Preson1: "<< Person1.FullName()<< endl;
+
+    cout << Person1.Function3() << endl;
 
 
     return 0;
