7271638ba1e423003067e427abc39c098581fa89
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-21 20:48:06 +0300

    Add

--- Part 1 ( Concepts )/2 - Classes And Mymory/07.cpp
+++ Part 1 ( Concepts )/2 - Classes And Mymory/07.cpp
@@ -2,4 +2,66 @@
 
 Read Only Property:
 
+A read-only property is a property that can only be read (accessed) but cannot
+be modified (written to) after it has been initialized. In C++, you can create
+a read-only property by providing only a getter function and omitting the
+setter function. This way, the value of the property can be accessed but not
+changed directly.
+
 */
+#include <iostream> 
+using namespace std;
+
+class clsPerson
+{
+
+private:
+    string _FirstName;
+    string _LastName;
+
+public:
+
+    void setFirstName(string FirstName)// Set
+    {
+        _FirstName = FirstName;
+
+    }
+
+    string FirstName()// Get
+    {
+        return _FirstName;
+    }
+
+    void setLastName(string LastName)// Set
+    {
+        _LastName = LastName;
+    }
+
+    string LastName()// Get
+    {
+        return _LastName;
+    }
+
+    
+    string FullName()
+    {
+        return FirstName() + " " + LastName();
+    }
+    
+
+};
+
+int main()
+{
+    clsPerson Person1;
+
+    Person1.setFirstName("Islam");
+    Person1.setLastName("Raad");
+
+    cout << "First Name: " << Person1.FirstName() << endl;
+    cout << "Last Name: " << Person1.LastName() << endl;
+    cout << "Full Name: "<< Person1.FullName()<< endl;
+
+
+    return 0;
+}
