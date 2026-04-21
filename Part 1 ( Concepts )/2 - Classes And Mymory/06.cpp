82cd2a457972eab03de9b5f8e6f7d827862c150e
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-21 20:14:40 +0300

    Add

--- Part 1 ( Concepts )/2 - Classes And Mymory/06.cpp
+++ Part 1 ( Concepts )/2 - Classes And Mymory/06.cpp
@@ -1,5 +1,11 @@
 /*
 
+Properties Get / Set:
 
+In C++, properties are not a built-in language feature like in some other
+programming languages (e.g., C#). However, you can achieve similar
+functionality using getter and setter functions. These functions allow you to
+control access to class members (variables) and provide a way to read and
+modify their values while maintaining encapsulation.
 
 */
