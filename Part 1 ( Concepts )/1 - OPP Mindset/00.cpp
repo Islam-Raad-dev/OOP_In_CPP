da9034b47d16e0b0da48597079f1ba5805cf2114
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-21 11:27:33 +0300

    What Is OOP And Why

--- Part 1 ( Concepts )/1 - OPP Mindset/00.cpp
+++ Part 1 ( Concepts )/1 - OPP Mindset/00.cpp
@@ -1,7 +1,20 @@
 /*
 
-What Is OOP And Why.
+What Is OOP And Why:
 
 OOP: A Simple Way To Organize Code In Big Projects.
+
+And OOP: A Programming Paradigm That Uses Objects And Classes To Organize Code.
+
+And There Are Many Benefits Like:
+
+1. Code Reusability: You Can Create A Class Once And Use It Multiple Times To Create Objects, Which Saves Time And Effort.
+
+2. Encapsulation: You Can Hide The Internal Details Of A Class And Only Expose What Is Necessary, Which Makes Your Code More Secure And Easier To Maintain.
+
+3. Inheritance: You Can Create A New Class That Inherits The Properties And Methods Of An Existing Class, Which Promotes Code Reusability And Makes It Easier To Create New Functionality.
+
+4. Polymorphism: You Can Use A Single Interface To Represent Different Types Of Objects, Which Makes Your Code More Flexible And Easier To Extend.
+
 
 */
