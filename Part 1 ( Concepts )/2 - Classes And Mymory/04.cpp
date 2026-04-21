f8d599ca0bb99a2031a64b84eafdafc0581ff3a9
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-21 11:51:07 +0300

    Add

--- Part 1 ( Concepts )/2 - Classes And Mymory/04.cpp
+++ Part 1 ( Concepts )/2 - Classes And Mymory/04.cpp
@@ -5,3 +5,35 @@ Object In Memory:
 When We Create An Object From A Class, The Object Is Stored In Memory. The Memory Used By The Object Depends On The Data Members Defined In The Class.
 
 */
+#include <iostream> 
+using namespace std;
+
+class clsPerson 
+{
+
+private:
+    short Age;
+
+public:
+    string FirstName;
+    string LastName;
+
+    string FullName()
+    {
+        return FirstName + " " + LastName;
+    }
+
+};
+
+int main()
+{
+    clsPerson Person1;
+
+    Person1.FirstName = "Islam";
+    Person1.LastName = "Raad";
+
+    cout << Person1.FullName() << endl;
+
+
+    return 0;
+}
