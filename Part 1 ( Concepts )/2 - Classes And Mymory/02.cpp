43c67464901aed3115f7c7f7391b3ca167d3d3fd
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-21 11:30:33 +0300

    Add

--- Part 1 ( Concepts )/2 - Classes And Mymory/02.cpp
+++ Part 1 ( Concepts )/2 - Classes And Mymory/02.cpp
@@ -2,4 +2,8 @@
 
 Classes And Objects:
 
+Classes: A Blueprint For Creating Objects That Defines The Properties And Behaviors Of The Objects.
+
+Objects: An Instance Of A Class That Contains The Data And Functions Defined In The Class.
+
 */
