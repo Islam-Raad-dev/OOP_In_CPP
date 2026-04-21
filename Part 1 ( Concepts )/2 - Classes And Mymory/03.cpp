bd84df9762614b6c6fdc81367f5178ac4d5e232c
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-21 11:47:09 +0300

    Add

--- Part 1 ( Concepts )/2 - Classes And Mymory/03.cpp
+++ Part 1 ( Concepts )/2 - Classes And Mymory/03.cpp
@@ -3,3 +3,35 @@
 Class Members:
 
 */
+#include <iostream> 
+using namespace std;
+
+class clsPerson 
+{
+
+private:
+    int Age;
+
+public:
+    string FirstName;
+    string LastName;
+
+    string FullName()
+    {
+        return FirstName + " " + LastName;
+    }
+
+};
+
+int main()
+{
+    clsPerson Person1;
+
+    Person1.FirstName = "Islam";
+    Person1.LastName = "Raad";
+
+    cout << Person1.FullName() << endl;
+
+
+    return 0;
+}
