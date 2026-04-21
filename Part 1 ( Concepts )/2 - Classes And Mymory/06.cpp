444fe2e1a80e0e9ad5e7248eb298e84912033fe9
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-21 20:15:02 +0300

    Add

--- Part 1 ( Concepts )/2 - Classes And Mymory/06.cpp
+++ Part 1 ( Concepts )/2 - Classes And Mymory/06.cpp
@@ -9,3 +9,59 @@ control access to class members (variables) and provide a way to read and
 modify their values while maintaining encapsulation.
 
 */
+#include <iostream> 
+using namespace std;
+
+class clsPerson
+{
+
+private:
+
+    int Variabl1 = 5;
+
+    int Function1()
+    {
+        return 40;
+    }
+
+protected:
+
+    int Variabl2 = 100;
+
+    int Function2()
+    {
+        return 50;
+    }
+
+public:
+
+    string FirstName;
+    string LastName;
+
+    string FullName()
+    {
+        return FirstName + " " + LastName;
+    }
+
+    float Function3()
+    {
+        return Function1() * Variabl1 * Variabl2;
+    }
+    
+
+};
+
+int main()
+{
+    clsPerson Person1;
+
+    Person1.FirstName = "Islam";
+    Person1.LastName = "Raad";
+
+    cout << "Preson1: "<< Person1.FullName()<< endl;
+
+    cout << Person1.Function3() << endl;
+
+
+    return 0;
+}
