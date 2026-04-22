ca092ec1ecbd17b391df87db403bdd696651dd50
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-22 19:36:15 +0300

    Add

--- Part 1 ( Concepts )/3 - Encapulation And Abstraction/12.cpp
+++ Part 1 ( Concepts )/3 - Encapulation And Abstraction/12.cpp
@@ -3,3 +3,83 @@
 Copy Constructor:
 
 */
+#include <iostream>
+using namespace std;
+
+class clsAddress
+{
+private:
+    string _AddresLine1;
+    string _AddressLine2;
+    string _POBox;
+    string _ZipCode;
+
+public:
+
+    clsAddress(string AddressLine1, string AddressLine2, string POBox, string ZipCode)
+    {
+        _AddresLine1 = AddressLine1;
+        _AddressLine2 = AddressLine2;
+        _POBox = POBox;
+        _ZipCode = ZipCode;
+    }
+
+    void SetAddressLine1(string AddressLine1)
+    {
+        _AddresLine1 = AddressLine1;
+    }
+
+    string AddressLine1()
+    {
+        return _AddresLine1;
+    }
+
+    void SetAddressLine2(string AddressLine2)
+    {
+        _AddressLine2 = AddressLine2;
+    }
+
+    string AddressLine2()
+    {
+        return _AddressLine2;
+    }
+
+    void SetPOBox(string POBox)
+    {
+        _POBox = POBox;
+    }
+
+    string POBox()
+    {
+        return _POBox;
+    }
+
+    void SetZipCode(string ZipCode)
+    {
+        _ZipCode = ZipCode;
+    }
+
+    string ZipCode()
+    {
+        return _ZipCode;
+    }
+
+    void Print()
+    {
+        cout <<"Address Details: "<< endl;
+        cout << "----------------" << endl;
+        cout << "Address Line 1 : " << _AddresLine1 << endl;
+        cout << "Address Line 2 : " << _AddressLine2 << endl;
+        cout << "PO Box         : " << _POBox << endl;
+        cout << "Zip Code       : " << _ZipCode << endl;
+    }
+};
+
+int main()
+{
+    clsAddress Address("14 Main St", "Apt 4B", "PO Box 123", "41001");
+
+    Address.Print();
+
+    return 0;
+}
