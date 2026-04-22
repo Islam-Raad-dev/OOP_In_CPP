1e09675d154d8635c2ff3495ac5eec2677745990
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-22 11:56:54 +0300

    Add

--- Part 1 ( Concepts )/3 - Encapulation And Abstraction/10.cpp
+++ Part 1 ( Concepts )/3 - Encapulation And Abstraction/10.cpp
@@ -9,6 +9,66 @@ using namespace std;
 
 class clsPerson
 {
-    
 
+private:
+    string _FirstName;
+    string _LastName;
+    string _Phone;
+
+public:
+    void setFirstName(string FirstName)
+    {
+        _FirstName = FirstName;
+    }
+
+    string GetFirstName()
+    {
+        return _FirstName;
+    }
+
+    void setLastName(string LastName)
+    {
+        _LastName = LastName;
+    }
+
+    string GetLastName()
+    {
+        return _LastName;
+    }
+
+    void setPhone(string Phone)
+    {
+        _Phone = Phone;
+    }
+
+    string GetPhone()
+    {
+        return _Phone;
+    }
+
+    void PrintInfo()
+    {
+        cout << "\nInfo:";
+        cout << "\n___________________";
+        cout << "\nFirstName: " << _FirstName;
+        cout << "\nLastName : " << _LastName;
+        cout << "\nFull Name: " << _FirstName << " " << _LastName;
+        cout << "\nPhone    : " << _Phone;
+        cout << "\n___________________\n";
+    }
+
+
+};
+
+int main()
+{
+    clsPerson Person1;
+
+    Person1.setFirstName("Islam");
+    Person1.setLastName("Raad");
+    Person1.setPhone("123456789");
+
+    Person1.PrintInfo();
+
+    return 0;
 }
