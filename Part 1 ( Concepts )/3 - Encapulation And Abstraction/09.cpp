dd06a610a055fe848652e20831e478ce4a6b3dcb
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-22 11:57:34 +0300

    First Principle : Encapsulation

---
+++ Part 1 ( Concepts )/3 - Encapulation And Abstraction/09.cpp
@@ -0,0 +1,41 @@
+/*
+
+First Principle : Encapsulation
+ 
+*/
+
+#include <iostream>
+using namespace std;
+
+class clsPerson
+{
+
+private:
+
+    string _FirstName;
+
+public:
+
+    void setFirstName(string FirstName)// Set
+    {
+        _FirstName = FirstName;
+
+    }
+
+    string GetFirstName()// Get
+    {
+        return _FirstName;
+    }
+
+};
+
+int main()
+{
+    clsPerson Person1;
+
+    Person1.setFirstName("Islam");
+    cout << Person1.GetFirstName() << endl;
+
+
+    return 0;
+}
