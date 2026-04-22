aa2da7ebb36887649ed9d0165e3adcfe95aa452e
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-22 20:14:36 +0300

    Add

--- Part 1 ( Concepts )/3 - Encapulation And Abstraction/15.cpp
+++ Part 1 ( Concepts )/3 - Encapulation And Abstraction/15.cpp
@@ -3,3 +3,30 @@
 Static Functions (Static Methods)
 
 */
+#include <iostream>
+using namespace std;
+
+class clsA
+{
+
+public:
+
+    static int Function()
+    {
+        return 10;
+    }
+
+    static int Function2()
+    {
+        return 20;
+    }
+};
+
+int main()
+{
+    cout << clsA::Function() << endl;
+    cout << clsA::Function2() << endl;
+
+    return 0;
+
+}
