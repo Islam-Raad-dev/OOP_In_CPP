e44289b067a1484ee778faef0811b109742d4b72
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-22 12:22:24 +0300

    Add

--- Part 1 ( Concepts )/6 - Project's/01 - Calculator.cpp
+++ Part 1 ( Concepts )/6 - Project's/01 - Calculator.cpp
@@ -12,10 +12,10 @@ class clsCalculator
 {
 private:
 
-    float _Result = 0;
-    float _LastNumber = 0;
-    string _LastOperation = "Clear";
-    float _PrevoiusResult = 0;
+    float Result = 0;
+    float LastNumber = 0;
+    string LastOperation = "Clear";
+    float PrevoiusResult = 0;
 
     bool _IsZero(float Number){
         return (Number == 0);
@@ -26,57 +26,68 @@ public:
 
     void Add(float Number)
     {
-        _LastNumber = Number;
-        _PrevoiusResult = _Result;
-        _LastOperation = "Adding";
-        _Result += Number;
+        LastNumber = Number;
+        PrevoiusResult = Result;
+        LastOperation = "Adding";
+        Result += Number;
     }
 
     void Subtract(float Number)
     {
-        _LastNumber = Number;
-        _PrevoiusResult = _Result;
-        _LastOperation = "Subtracting";
-        _Result -= Number;
+        LastNumber = Number;
+        PrevoiusResult = Result;
+        LastOperation = "Subtracting";
+        Result -= Number;
     }
 
     void Multiply(float Number)
     {
-        _LastNumber = Number;
-        _PrevoiusResult = _Result;
-        _LastOperation = "Multiplying";
-        _Result *= Number;
+        LastNumber = Number;
+        PrevoiusResult = Result;
+        LastOperation = "Multiplying";
+        Result *= Number;
     }
 
     void Divide(float Number)
     {
+        
+        LastNumber = Number;
+
         if (_IsZero(Number))
         {
-            return;
+            Number = 1;
         }
-        _LastNumber = Number;
-        _PrevoiusResult = _Result;
-        _LastOperation = "Dividing";
-        _Result /= Number;
+
+        PrevoiusResult = Result;
+        LastOperation = "Dividing";
+        Result /= Number;
     }
 
     void Clear()
     {
+        LastNumber = 0;
+        PrevoiusResult = 0;
+        LastOperation = "Clear";
+        Result = 0;
 
     }
-    
+
+    float GetFinalResult()
+    {
+        return Result;
+    }
     void CancelLastOpertion()
     {
-        _LastNumber = 0;
-        _LastOperation = "Cancelling Last Operation";
-        _Result = _PrevoiusResult;
+        LastNumber = 0;
+        LastOperation = "Cancelling Last Operation";
+        Result = PrevoiusResult;
 
     }
 
     void PrintResult()
     {
         cout << "Result ";
-        cout << "After " << _LastOperation << " " << _LastNumber << "is: " << _Result;
+        cout << "After " << LastOperation << " " << LastNumber << "is: " << Result;
 
     }
 };
@@ -86,8 +97,11 @@ int main()
 
     clsCalculator Calculator1;
 
-    Calculator1.
+    Calculator1.Clear();
+
     Calculator1.Add(10);
+
+    Calculator1.Clear();
 
     return 0;
 }
