3f0cd0947e7ebd460c56a1a2c6894cf39b3f010c
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-22 19:28:47 +0300

    Add

--- Part 1 ( Concepts )/3 - Encapulation And Abstraction/11.cpp
+++ Part 1 ( Concepts )/3 - Encapulation And Abstraction/11.cpp
@@ -2,6 +2,19 @@
 
 Constructors:
 
+A constructor is a special member function of a class that is automatically
+called when an object of the class is created. It is used to initialize the
+object's data members and set up any necessary resources. Constructors have
+the same name as the class and do not have a return type, not even void. They
+can be defined with parameters to allow for different ways of initializing
+objects. If no constructor is explicitly defined, the compiler provides a
+default constructor that initializes data members to default values
+Constructors can also be overloaded, meaning that a class can have multiple
+constructors with different parameter lists to provide various ways of
+initializing objects. The main purpose of a constructor is to ensure that an
+object is in a valid state when it is created, and to simplify the process of
+object initialization for the programmer.
+
 */
 #include <iostream>
 using namespace std;
@@ -23,7 +36,7 @@ public:
         _POBox = "N/A";
         _ZipCode = "N/A";
     }
-    
+
     void SetAddressLine1(string AddressLine1)
     {
         _AddresLine1 = AddressLine1;
