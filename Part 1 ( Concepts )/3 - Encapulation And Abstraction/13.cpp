a12f4753bbc80d38348aba343f54ecd6e131c63d
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-22 19:52:32 +0300

    Add

--- Part 1 ( Concepts )/3 - Encapulation And Abstraction/13.cpp
+++ Part 1 ( Concepts )/3 - Encapulation And Abstraction/13.cpp
@@ -5,6 +5,39 @@ Destructors:
 A destructor is a special member function of a class that is executed when an
 object of that class goes out of scope or is explicitly deleted. The purpose
 of a destructor is to perform any necessary cleanup tasks, such as releasing
-resources or memory that the object may have acquired during its lifetime. 
+resources or memory that the object may have acquired during its lifetime.
 
 */
+#include <iostream>
+using namespace std;
+
+class clsPerson
+{
+private:
+    string _FullName;
+
+public:
+    // Constructor
+    clsPerson()
+    {
+        cout << "Hi, I'm Constructor" << endl;
+    }
+
+    // Destructor
+    ~clsPerson()
+    {
+        cout << "Hi, I'm Destructor" << endl;
+    }
+};
+
+void Function()
+{
+    clsPerson Person1;
+}
+
+int main()
+{
+    Function();
+
+    return 0;
+};
