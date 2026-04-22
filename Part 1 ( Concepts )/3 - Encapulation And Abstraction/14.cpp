9dec7df83dc6ef43d536280a6f5d38d5b6d3c397
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-22 20:05:49 +0300

    Add

--- Part 1 ( Concepts )/3 - Encapulation And Abstraction/14.cpp
+++ Part 1 ( Concepts )/3 - Encapulation And Abstraction/14.cpp
@@ -3,3 +3,44 @@
 Static Members
 
 */
+#include <iostream>
+#include <string>
+using namespace std;
+
+class clsA
+{
+
+public:
+
+    int var;
+    static int counter;
+
+    clsA()
+    {
+        counter++;
+    }
+
+    void Print(){
+        cout << "\nvar: " << var ;
+        cout << "\n counter: " << counter << endl;
+    }
+
+
+};
+
+int main()
+{
+    clsA a1;
+    a1.var = 10;
+    a1.Print();
+
+    clsA a2;
+    a2.var = 20;
+    a2.Print();
+
+    clsA a3;
+    a3.var = 30;
+    a3.Print();
+
+    return 0;
+};
