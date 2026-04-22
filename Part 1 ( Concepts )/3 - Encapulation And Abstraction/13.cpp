86d68d9e61ee5302d4a3f33a0321bf64eacfaf1e
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-22 19:45:18 +0300

    Add

--- Part 1 ( Concepts )/3 - Encapulation And Abstraction/13.cpp
+++ Part 1 ( Concepts )/3 - Encapulation And Abstraction/13.cpp
@@ -1,5 +1,10 @@
 /*
 
-Destrutors: 
+Destrutors:
+
+A destructor is a special member function of a class that is executed when an
+object of that class goes out of scope or is explicitly deleted. The purpose
+of a destructor is to perform any necessary cleanup tasks, such as releasing
+resources or memory that the object may have acquired during its lifetime. 
 
 */
