6882a6a8896a7d40b2a684e8c7d46998f1b3ba98
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-25 11:49:42 +0300

    Add

---
+++ Part 1 ( Concepts )/3 - Encapulation And Abstraction/16.cpp
@@ -0,0 +1,113 @@
+/*
+
+Person Exercise
+
+*/
+#include <iostream>
+using namespace std;
+
+class clsPerson
+{
+private:
+    string _FirstName;
+    string _LastName;
+    string _Phone;
+    string _Email;
+    int _ID;
+
+public:
+
+    void SetFirstName(string FirstName)
+    {
+        _FirstName = FirstName;
+    }
+
+    string GetFirstName()
+    {
+        cout << _FirstName << endl;
+        return _FirstName;
+    }
+    void SetLastName(string LastName)
+    {
+        _LastName = LastName;
+    }
+
+    string GetLastName()
+    {
+        cout << _LastName << endl;
+        return _LastName;
+    }
+
+    void SetPhone(string Phone)
+    {
+        _Phone = Phone;
+    }
+
+    string GetPhone()
+    {
+        cout << _Phone << endl;
+        return _Phone;
+    }
+
+    void SetEmail(string Email)
+    {
+        _Email = Email;
+        
+    }
+
+    string GetEmail()
+    {
+        cout << _Email << endl;
+        return _Email;
+    }
+
+    void SetID(int ID)
+    {
+        _ID = ID;
+    }
+
+    int GetID()
+    {
+        cout << _ID << endl;
+        return _ID;
+    
+    }
+
+    void SendEmail(string Message)
+    {
+        cout << "The Folloing Message "<< Message << " Sent Seccesfully To Email:" << _Email << endl;
+    }
+
+    void SendSMS()
+    {
+        cout << "Sending SMS..." << endl;
+    }
+
+
+    void PrintInfo()
+    {
+        cout << "INFO :" << endl;
+        cout << "______________________________" << endl;
+        cout << "First Name: " << _FirstName ;
+        cout << "\nLast Name: " << _LastName ;
+        cout << "\nPhone: " << _Phone;
+        cout << "\nEmail: " << _Email;
+        cout << "\nID: " << _ID ;
+        cout << "\n______________________________" << endl;
+
+    }
+
+};
+
+int main(){
+
+    clsPerson Person1;
+
+    Person1.PrintInfo();
+
+    Person1.SendEmail();
+    Person1.SendSMS();
+
+    return 0;
+
+}
