5846b476658440143d3dfa9bd14e0cfc3d92f3e4
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-30 18:15:30 +0300

    Add

--- Part 1 ( Concepts )/4 - Inheritance And Polymorphism/22.cpp
+++ Part 1 ( Concepts )/4 - Inheritance And Polymorphism/22.cpp
@@ -3,3 +3,239 @@
 Mulit Level Inheritance
 
 */
+/*
+
+Function Overriding
+ 
+*/
+
+#include<iostream>  
+using namespace std; 
+
+class clsPerson
+{
+    
+private:
+    string _FirstName;
+    string _LastName;
+    string _Phone;
+    string _Email;
+    int _ID;
+
+
+public:
+
+
+    clsPerson(int ID, string FirstName, string LastName, string Phone, string Email)
+    {
+        _ID = ID;
+        _FirstName = FirstName;
+        _LastName = LastName;
+        _Phone = Phone;
+        _Email = Email;
+    }
+
+    void SetFirstName(string FirstName)
+    {
+        _FirstName = FirstName;
+    }
+
+    string FirstName()
+    {
+        return _FirstName;
+    }
+    void SetLastName(string LastName)
+    {
+        _LastName = LastName;
+    }
+
+    string LastName()  
+    {
+
+        return _LastName;
+    }
+
+    string FullName()
+    {
+        return _FirstName + " " + _LastName;
+    }
+
+
+    void SetPhone(string Phone)
+    {
+        _Phone = Phone;
+    }
+
+    string Phone()
+    {
+
+        return _Phone;
+    }
+
+    void SetEmail(string Email)
+    {
+        _Email = Email;
+        
+    }
+
+    string Email()
+    {
+
+        return _Email;
+    }
+
+    void SetID(int ID)
+    {
+        _ID = ID;
+    }
+
+    int ID()
+    {
+
+        return _ID;
+    
+    }
+
+
+    void SendEmail(string Subject, string Body)
+    {
+        cout << "\n\nThe Folloing Message Sent Seccesfully To Email:" << _Email << endl;
+        cout << "Subject: " << Subject << endl;
+        cout << "Body: " << Body << endl;
+    
+    }
+
+    void SendSMS(string TextMessage)
+    {
+        cout << "\n\nThe Folloing Message Sent Seccesfully To Phone: " << _Phone << "\n" << endl;
+        cout << TextMessage << endl;
+    }
+
+
+    void PrintInfo()
+    {
+        cout << "Info :" << endl;
+        cout << "______________________________" << endl;
+        cout << "\nFirst Name: " << _FirstName ;
+        cout << "\nLast Name: " << _LastName ;
+        cout << "\nFull Name: " << FullName();
+        cout << "\nPhone: " << _Phone;
+        cout << "\nEmail: " << _Email;
+        cout << "\nID: " << _ID ;  
+        cout << "\n______________________________\n" << endl;
+
+    }
+
+};
+
+class clsEmployee : public clsPerson
+{
+
+private:
+
+    string _Title;
+    string _Department;
+    double _Salary;
+
+public:
+
+    clsEmployee(int ID, string FirstName, string LastName, string Phone, string Email, string Title, string Department, double Salary) 
+        : clsPerson(ID, FirstName, LastName, Phone, Email)
+    {
+        _Title = Title;
+        _Department = Department;
+        _Salary = Salary;
+
+    }
+
+    void SetTitle(string Title)
+    {
+        _Title = Title;
+    }
+
+    string Title()
+    {
+    
+        return _Title;
+    }
+
+    void SetDepartment(string Department)
+    {
+        _Department = Department;
+    }
+
+    string Department()
+    {
+    
+        return _Department;
+    }
+
+    void SetSalary(double Salary)
+    {
+        _Salary = Salary;
+    }
+
+    double Salary()
+    {
+    
+        return _Salary;
+    }
+
+    void PrintInfo()
+    {
+        cout << "Info :" << endl;
+        cout << "______________________________" << endl;
+        cout << "\nFirst Name: " << FirstName() ;
+        cout << "\nLast Name: " << LastName() ;
+        cout << "\nFull Name: " << FullName();
+        cout << "\nPhone: " << Phone();
+        cout << "\nEmail: " << Email();
+        cout << "\nID: " << ID();
+        cout << "\nTitle: " << _Title;
+        cout << "\nDepartment: " << _Department;
+        cout << "\nSalary: " << _Salary;
+        cout << "\n______________________________\n" << endl;
+    }
+
+}; 
+
+class clsDeveloper : public clsEmployee
+{
+    
+private:
+    string _MainProgrammingLanguage;
+
+public:
+    clsDeveloper(int ID, string FirstName, string LastName, string Phone, string Email, string Title, string Department, double Salary, string MainProgrammingLanguage)
+        : clsEmployee(ID, FirstName, LastName, Phone, Email, Title, Department, Salary)
+    {
+        _MainProgrammingLanguage = MainProgrammingLanguage;
+    }
+
+    void SetMainProgrammingLanguage(string MainProgrammingLanguage)
+    {
+        _MainProgrammingLanguage = MainProgrammingLanguage;
+    }
+
+    string MainProgrammingLanguage()  
+    {
+
+    }
+
+    void PrintInfo()
+    {
+        
+    };
+
+
+};
+int main()
+{ 
+    clsDeveloper Developer1(10, "Islam", "Raad", "07718277643", "islamraad43", "ML Ops", "Artificial Intelligence", 4000, "C++");
+
+    Developer1.PrintInfo();
+ 
+
+    return 0;  
+
+}
