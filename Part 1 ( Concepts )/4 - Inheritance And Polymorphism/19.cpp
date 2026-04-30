9f37dc3b2dfd2653036644dfe894ad223cacf2fc
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-30 17:44:34 +0300

    Add

--- Part 1 ( Concepts )/4 - Inheritance And Polymorphism/19.cpp
+++ Part 1 ( Concepts )/4 - Inheritance And Polymorphism/19.cpp
@@ -183,6 +183,7 @@ int main()
     clsEmployee Employee1(10, "Islam", "Raad", "07718277643", "islamraad43", "ML Ops", "Artificial Intelligence", 4000);
 
     Employee1.PrintInfo();
+    cout << Employee1.Salary() << endl;
 
 
     return 0;  
