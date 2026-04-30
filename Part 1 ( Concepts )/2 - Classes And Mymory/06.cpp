17b83a8f588348c1670c05a27397932dcd97138c
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-04-30 18:18:53 +0300

    Properties Get / Set

---
+++ Part 1 ( Concepts )/2 - Classes And Mymory/06.cpp
@@ -0,0 +1,67 @@
+/*
+
+Properties Get / Set: 
+
+In C++, properties are not a built-in language feature like in some other
+programming languages (e.g., C#). However, you can achieve similar
+functionality using getter and setter functions. These functions allow you to
+control access to class members (variables) and provide a way to read and
+modify their values while maintaining encapsulation.
+
+*/
+#include <iostream> 
+using namespace std;
+
+class clsPerson
+{
+
+private:
+    string _FirstName;
+    string _LastName;
+
+public:
+
+    void setFirstName(string FirstName)// Set
+    {
+        _FirstName = FirstName;
+
+    }
+
+    string FirstName()// Get
+    {
+        return _FirstName;
+    }
+
+    void setLastName(string LastName)// Set
+    {
+        _LastName = LastName;
+    }
+
+    string LastName()// Get
+    {
+        return _LastName;
+    }
+
+    
+    string FullName()
+    {
+        return FirstName() + " " + LastName();
+    }
+    
+
+};
+
+int main()
+{
+    clsPerson Person1;
+
+    Person1.setFirstName("Islam");
+    Person1.setLastName("Raad");
+
+    cout << "First Name: " << Person1.FirstName() << endl;
+    cout << "Last Name: " << Person1.LastName() << endl;
+    cout << "Full Name: "<< Person1.FullName()<< endl;
+
+
+    return 0;
+}
