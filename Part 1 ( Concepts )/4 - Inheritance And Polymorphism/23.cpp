95bc2cc65e405afd85019c2ee630403258f94e57
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-03 11:32:14 +0300

    Add

--- Part 1 ( Concepts )/4 - Inheritance And Polymorphism/23.cpp
+++ Part 1 ( Concepts )/4 - Inheritance And Polymorphism/23.cpp
@@ -9,9 +9,36 @@ using namespace std;
 class clsA
 {
 
+private:
+
+    int Var1;
+    void Function1()
+    {
+        cout << "Function 1.";
+    }
+
+protected:
+
+    int Var1;
+    void Function2()
+    {
+        cout << "Function 2.";
+    }
+
+public:
+
+    
+
 };
 
 class clsB
 {
 
+
 };
+
+int main()
+{
+
+    return 0;
+}
