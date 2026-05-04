8b3aa8ac36c1aaf715c0cfa7562a862dfcfab33e
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-04 18:59:40 +0300

    Employee Exercise

--- Part 1 ( Concepts )/3 - Encapulation And Abstraction/17.cpp
+++ Part 1 ( Concepts )/3 - Encapulation And Abstraction/17.cpp
@@ -11,14 +11,14 @@ class clsEmployee
 
 private:
 
-    int _ID ;
-    string _FirstName ;
-    string _LastName ;
-    string _Title ;
-    double _Salary ;
-    string _Department ;
-    string _Phone ;
-    string _Email ;
+    int _ID ; 
+    string _FirstName ; 
+    string _LastName ; 
+    string _Title ; 
+    double _Salary ; 
+    string _Department ; 
+    string _Phone ; 
+    string _Email ; 
 
 public:
 
