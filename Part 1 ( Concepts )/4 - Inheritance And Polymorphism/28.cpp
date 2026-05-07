8286d5b73dc76412b0f9fceca367e18a220069d8
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-07 17:15:20 +0300

    Add

--- Part 1 ( Concepts )/4 - Inheritance And Polymorphism/28.cpp
+++ Part 1 ( Concepts )/4 - Inheritance And Polymorphism/28.cpp
@@ -7,8 +7,48 @@ Static Early Binding Vs Dynamic Late Binding
 #include<iostream>
 using namespace std;
 
+class clsPerson
+{
+
+public:
+    virtual void Print()
+    {
+        cout << "Hi, I'm A Perosn\n";
+    }
+};
+
+class clsEmployee : public clsPerson
+{
+
+public:
+    void Print()
+    {
+        cout << "Hi, I'm A Employee!\n";
+    }
+};
+
+class clsStudent : public clsPerson
+{
+
+public:
+    void Print()
+    {
+        cout << "Hi, I'm A Student!\n";
+    }
+};
 int main()
 {
+    clsEmployee Employee1;
+    clsStudent Student1;
+
+    Employee1.Print();
+    Student1.Print();
+
+    clsPerson * Person1 = &Employee1;
+    clsPerson * Peront2 = &Student1;
+
+    Person1->Print();
+    Peront2->Print();
 
     return 0;
 }
