a89bba1319ed4011aae4e59b7f595b24f33c73a5
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-07 15:57:07 +0300

    Add

--- Part 1 ( Concepts )/4 - Inheritance And Polymorphism/24.cpp
+++ Part 1 ( Concepts )/4 - Inheritance And Polymorphism/24.cpp
@@ -8,7 +8,50 @@ Inheritance Visibility Modes:
 #include <iostream>
 using namespace std;
 
-class A
+class clsA
 {
 
+private:
+
+    int V1;
+    int Fun1(){
+        return 1;
+    }
+
+protected:
+
+    int V2;
+    int Fun2(){
+        return 2;
+    }
+
+public:
+
+    int V3;
+    int Fun3(){
+        return 3;
+    }
+
 };
+
+class clsB : public clsA
+{
+
+public:
+
+    int V4;
+    int Fun4(){
+        return 4;
+    }
+
+
+};
+
+int main()
+{
+    clsA A;
+
+    A.
+
+    return 0;
+}
