57dcfbbb287d868edfc7b0d3339443c1cb7b4c04
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-07 17:28:33 +0300

    Add

--- Part 1 ( Concepts )/4 - Inheritance And Polymorphism/30.cpp
+++ Part 1 ( Concepts )/4 - Inheritance And Polymorphism/30.cpp
@@ -3,3 +3,32 @@
 Pure Virtual Functions and Abstract Classes
 
 */
+
+#include <iostream>
+using namespace std;
+
+class clsPerson
+{
+
+public:
+    virtual void Print() = 0;
+};
+
+class clsEmployee : public clsPerson
+{
+
+public:
+    void Print()
+    {
+        cout << "Hi, I'm A Employee!\n";
+    }
+};
+
+int main()
+{
+
+    clsEmployee Employee1;
+    Employee1.Print();
+
+    return 0;
+}
