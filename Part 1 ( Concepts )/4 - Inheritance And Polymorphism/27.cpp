36606a74cf8142af17c18f673fa4830c10c79333
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-07 17:02:13 +0300

    Add

--- Part 1 ( Concepts )/4 - Inheritance And Polymorphism/27.cpp
+++ Part 1 ( Concepts )/4 - Inheritance And Polymorphism/27.cpp
@@ -11,4 +11,26 @@ using namespace std;
 class clsPerson
 {
 
+public:
+    void Print()
+    {
+        cout << "Hi I'm A Perosn\n";
+    }
 };
+
+class clsEmployee : public clsPerson
+{
+
+public:
+    void Print()
+    {
+        cout << "Hi I;m A Employee!\n";
+    }
+};
+
+int main()
+{
+
+
+    return 0;
+}
