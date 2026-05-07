6e546a8891ae66a95654d0d36b7daa3ee0be1b13
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-07 17:55:13 +0300

    SASS

--- Part 1 ( Concepts )/5 - Misc/32.cpp
+++ Part 1 ( Concepts )/5 - Misc/32.cpp
@@ -3,3 +3,30 @@
 Friend Functions
 
 */
+#include <iostream>
+using namespace std;
+
+class clsA
+{
+
+private:
+
+    int _Var1;
+
+protected:
+
+    int Var3;
+
+public:
+
+    int Var2;
+    clsA()
+    {
+        _Var1 = 10;
+        Var2 = 20;
+    }
+
+    friend void Display(clsA A1);
+};
+
+void Display(clsA A1)
