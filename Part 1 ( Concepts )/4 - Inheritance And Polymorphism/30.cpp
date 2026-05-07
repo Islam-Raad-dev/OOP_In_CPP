3a92deac9574df5218f7ce1c2981823786434845
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-07 17:30:17 +0300

    Add

--- Part 1 ( Concepts )/4 - Inheritance And Polymorphism/30.cpp
+++ Part 1 ( Concepts )/4 - Inheritance And Polymorphism/30.cpp
@@ -7,4 +7,17 @@ Pure Virtual Functions and Abstract Classes
 #include <iostream>
 using namespace std;
 
-class cls
+class clsMobile
+{
+
+    virtual void Dial(string PhoneNumber) = 0;
+    virtual void SendSMS(string Text) = 0;
+    virtual void TakePicture() = 0;
+
+};
+
+int main()
+{
+
+    return 0;
+}
