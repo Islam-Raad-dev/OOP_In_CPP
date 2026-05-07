09f98ad221b48a11975dede03b6602394b11b042
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-07 17:47:11 +0300

    Edit

--- Part 1 ( Concepts )/5 - Misc/31.cpp
+++ Part 1 ( Concepts )/5 - Misc/31.cpp
@@ -1,5 +1,30 @@
 /*
 
-Frind Classes
+Friend Classes
 
 */
+#include <iostream>
+using namespace std;
+
+class clsA
+{
+
+private:
+
+    int _Var1;
+
+public:
+
+    int Var2;
+    clsA()
+    {
+        _Var1 = 10;
+        Var2 = 20;
+    }
+
+};
+
+class clsB 
+{
+
+};
