2e8aa48cf357836b2773f45947220e4310c16bcf
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-07 17:23:14 +0300

    Add

--- Part 1 ( Concepts )/4 - Inheritance And Polymorphism/29.cpp
+++ Part 1 ( Concepts )/4 - Inheritance And Polymorphism/29.cpp
@@ -3,3 +3,26 @@
 Fourth Principle Of OOP : Plymorphism
 
 */
+
+#include <iostream>
+using namespace std;
+
+class clsPerson
+{
+
+public:
+    virtual void Print()
+    {
+        cout << "Hi, I'm A Perosn\n";
+    }
+};
+
+class clsEmployee : public clsPerson
+{
+
+public:
+    void Print()
+    {
+        cout << "Hi, I'm A Employee!\n";
+    }
+};
