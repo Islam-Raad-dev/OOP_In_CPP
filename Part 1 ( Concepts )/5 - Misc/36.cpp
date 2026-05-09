7d92a410c990703087b86c8aa91837f3a0b3cdf6
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-09 15:54:10 +0300

    Edit

--- Part 1 ( Concepts )/5 - Misc/36.cpp
+++ Part 1 ( Concepts )/5 - Misc/36.cpp
@@ -10,64 +10,11 @@ Parameterized Constructor Of The Base Class
 
 */
 #include<iostream> 
-#include"clcPerson.h" 
+#include"clsPerson.h" 
+#include"clsEmployee.h"
 using namespace std; 
 
-
-class clsEmployee : public clsPerson
-{
-
-private:
-
-    string _Title;
-    string _Department;
-    double _Salary;
-
-public:
-
-    clsEmployee(int ID, string FirstName, string LastName, string Phone, string Email, string Title, string Department, double Salary) 
-        : clsPerson(ID, FirstName, LastName, Phone, Email)
-    {
-        _Title = Title;
-        _Department = Department;
-        _Salary = Salary;
-
-    }
-
-    void SetTitle(string Title)
-    {
-        _Title = Title;
-    }
-
-    string Title()
-    {
-    
-        return _Title;
-    }
-
-    void SetDepartment(string Department)
-    {
-        _Department = Department;
-    }
-
-    string Department()
-    {
-    
-        return _Department;
-    }
-
-    void SetSalary(double Salary)
-    {
-        _Salary = Salary;
-    }
-
-    double Salary()
-    {
-    
-        return _Salary;
-    }
-
-}; 
+ 
 
 int main()
 { 
