58dec0ce7cd121f9a88b54298c2e7920d6b80279
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-09 18:10:19 +0300

    Period Library

--- Part 1 ( Concepts )/6 - Project's/04.cpp
+++ Part 1 ( Concepts )/6 - Project's/04.cpp
@@ -3,3 +3,18 @@
 Period Library
 
 */
+
+#include<iostream>
+#include "clsPeriod.h"
+using namespace std;
+
+int main()
+{
+    clsPeriod Period1;
+    Period1.Print();
+
+    clsPeriod Period2;
+    Period2.Print();
+
+    return 0;
+}
