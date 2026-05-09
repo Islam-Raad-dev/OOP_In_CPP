6bfc2b2330977c945a393cdf6502b20ac25d91ef
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-09 16:30:08 +0300

    Add

--- Part 1 ( Concepts )/5 - Misc/38.cpp
+++ Part 1 ( Concepts )/5 - Misc/38.cpp
@@ -3,3 +3,48 @@
 Passing Oject To Function By(Rev / Val)
 
 */
+#include <iostream>
+using namespace std;
+
+class clsA
+{
+
+public:
+
+    int X;
+
+    void Print()
+    {
+        cout << "The Value Of X = " << X << endl;
+    
+    }
+
+    void Func1(clsA A1)
+    {
+        A1.X = 100;
+    }
+
+    void Func2(clsA &A1)
+    {
+        A1.X = 200;
+    }
+};
+
+int main()
+{
+
+    clsA A1;
+
+    A1.X = 50;
+
+    cout << "A Value Before Calling Function:\n";
+    A1.Print();
+
+
+    A1.Func1(A1);
+
+    cout << "\nA Value After Calling Function:\n";
+    A1.Print();
+
+    return 0;
+}
