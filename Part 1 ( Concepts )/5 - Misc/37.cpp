f33fe4e94dd83d8b0ad36481ec1831f298c6bdde
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-09 16:19:51 +0300

    Edit

--- Part 1 ( Concepts )/5 - Misc/37.cpp
+++ Part 1 ( Concepts )/5 - Misc/37.cpp
@@ -3,3 +3,52 @@
 This Pointer In C++
 
 */
+#include <iostream>
+using namespace std;
+
+class clsEmployee
+{
+
+public:
+
+    int ID;
+    string Name;
+    float Salary;
+
+    clsEmployee(int ID, string Name, float Salary)
+    {
+        this->ID = ID;
+        this->Name = Name;
+        this->Salary = Salary;
+    
+    }
+
+    static void Func1(clsEmployee Employee1)
+    {
+
+    }
+
+    void Func2()
+    {
+
+    }
+
+    void Print()
+    {
+
+        cout << ID << " " << Name << " " << Salary << endl;
+        cout << this->ID << " " << this->Name << " " << this->Salary << endl;
+
+    }
+};
+
+int main()
+{
+    clsEmployee Employee1(1212, "Islam Raad" , 3500.5);
+
+    Employee1.Print();
+
+
+
+    return 0;
+}
