fdb739a468481d29c3e9de40367433ef265e8127
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-09 16:39:46 +0300

    Object And Dynamic Array

--- Part 1 ( Concepts )/5 - Misc/40.cpp
+++ Part 1 ( Concepts )/5 - Misc/40.cpp
@@ -5,3 +5,48 @@ Object And Dynamic Array
 
 
 */
+#include <iostream>
+using namespace std;
+
+class clsA
+{
+
+public:
+
+    clsA()
+    {
+    }
+
+    clsA(int Value)
+    {
+        X = Value;
+    }
+
+    int X;
+
+    void Print()
+    {
+        cout << "The Value Of X = " << X << endl;
+    }
+};
+
+int main()
+{
+
+    int NumberOfObject = 5;
+    clsA *arrA = new clsA[NumberOfObject];
+
+    for (short i = 0; i < NumberOfObject; i++)
+    {
+        arrA[i] = clsA(i);
+    }
+
+    for (short i = 0; i < NumberOfObject; i++)
+    {
+        arrA[i].Print();
+    }
+
+    delete[] arrA;
+
+    return 0;
+}
