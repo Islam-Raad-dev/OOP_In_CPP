1d79a98876ab2b94d32b7112cd3127b9e7a4c817
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-09 18:07:26 +0300

    Date Library

--- Part 1 ( Concepts )/6 - Project's/03.cpp
+++ Part 1 ( Concepts )/6 - Project's/03.cpp
@@ -11,8 +11,17 @@ using namespace std;
 int main()
 {
     clsDate Date1;
-
     Date1.Print();
+
+    clsDate Date2("31/1/2026");
+    Date2.Print();
+
+    clsDate Date3(20, 12, 2023);
+    Date3.Print();
+
+    clsDate Date4(250, 2022);
+    Date4.Print();
+
 
     return 0;
 }
