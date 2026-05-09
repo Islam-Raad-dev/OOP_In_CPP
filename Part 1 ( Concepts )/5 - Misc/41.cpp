ff4499d55f7f354c77039c63db3ffd024b81adc1
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-09 16:46:48 +0300

    Add

--- Part 1 ( Concepts )/5 - Misc/41.cpp
+++ Part 1 ( Concepts )/5 - Misc/41.cpp
@@ -6,3 +6,22 @@ Object With Paramerterized Constuctor And Array
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class clsA
+{
+
+    public:
+
+    clsA(int Value)
+    {
+        X = Value;
+    }
+
+    int X;
+
+    void Print()
+    {
+        cout << "The Value Of X = " << X << endl;
+    }
+
+};
