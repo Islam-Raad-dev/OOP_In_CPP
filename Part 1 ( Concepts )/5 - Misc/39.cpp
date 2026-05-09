990cc62bc8c3bb97cd90212ec8b38bfcbb8dc5cc
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-09 16:33:35 +0300

    Add

--- Part 1 ( Concepts )/5 - Misc/39.cpp
+++ Part 1 ( Concepts )/5 - Misc/39.cpp
@@ -3,3 +3,27 @@
 Object And Vector
 
 */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class clsA
+{
+
+public:
+
+    clsA(int Value)
+    {
+        X = Value;
+    
+    }
+
+    int X;
+
+    void Print()
+    {
+        cout << "The Value Of X = " << X << endl;
+    }
+
+};
