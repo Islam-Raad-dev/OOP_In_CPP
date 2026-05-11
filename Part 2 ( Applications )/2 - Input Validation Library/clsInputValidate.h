8e2ed36b72816189442f70c22cb38c0102fdc6d2
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-11 18:45:21 +0300

    Edit

--- Part 2 ( Applications )/2 - Input Validation Library/clsInputValidate.h
+++ Part 2 ( Applications )/2 - Input Validation Library/clsInputValidate.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "clsString.h"
-#include "clsDate.h"
+#include<limits>
+#include <clsString.h>
+#include <clsDate.h>
 
 class clsInputValidate
 {
