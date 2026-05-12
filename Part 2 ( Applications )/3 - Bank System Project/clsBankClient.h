6de4776b2821fc31a93d02ed85c9d048b26df5d9
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-12 11:44:34 +0300

    Edit 5

--- Part 2 ( Applications )/3 - Bank System Project/clsBankClient.h
+++ Part 2 ( Applications )/3 - Bank System Project/clsBankClient.h
@@ -4,8 +4,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include<clsString.h>
-#include<clsPerson.h>
+#include <clsString.h>
+#include <clsPerson.h>
 
 using namespace std;
 
@@ -13,29 +13,27 @@ class clsBankClient : public clsPerson
 {
 
 private:
-
-    enum enMode {EmptyMode = 0, UpdateMode = 1};
+    enum enMode
+    {
+        EmptyMode = 0,
+        UpdateMode = 1
+    };
     enMode _Mode;
-
 
     string _AccountNumber;
     string _PinCode;
     float _AccountBalance;
 
-    static clsBankClient _ConvertLineToClientObject(string Line, string ass)
-    {}
-    static clsBankClient _ConvertClientObjectToLine(clsBankClient Client, string ass)
-    {}
+    static clsBankClient _ConvertLineToClientObject(string Line, string Seperator = "#//#")
+    {
+    }
+    static clsBankClient _ConvertClientObjectToLine(clsBankClient Client, string Seperator = "#//#")
+    {
+    }
 
     static clsBankClient _GetEmptyClientObject()
-    {}
-    static clsBankClient _GetUpdateClientObject()
-    {}
-
-
+    {
+    }
 
 public:
-
-
-
 };
