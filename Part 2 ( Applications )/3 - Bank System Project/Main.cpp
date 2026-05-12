ba30597031c8047c12d5b29a9e1a355863b36dbc
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-12 11:35:26 +0300

    Add

--- Part 2 ( Applications )/3 - Bank System Project/Main.cpp
+++ Part 2 ( Applications )/3 - Bank System Project/Main.cpp
@@ -10,28 +10,3 @@ Bank Project
 #include<clsUtil.h>
 using namespace std;
 
-class clsPerson
-{
-
-private:
-
-    string _FirstName;
-    string _LastName;
-    string _Email;
-    string _Phone;
-
-public:
-
-    clsPerson(string FirstName, string LastName, string Email, string Phone)
-    {
-        _FirstName = FirstName;
-        _LastName = LastName;
-        _Email = Email;
-        _Phone = Phone;
-    }
-
-    
-    
-
-
-};
