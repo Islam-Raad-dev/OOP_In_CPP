14dc9523dbe8db70632b59e259a29e076bb5df00
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-13 19:48:58 +0300

    Update Client Recorde

--- Part 2 ( Applications )/3 - Bank System Project/Main.cpp
+++ Part 2 ( Applications )/3 - Bank System Project/Main.cpp
@@ -5,16 +5,48 @@ Bank Project
 */
 #include <iostream>
 #include "clsBankClient.h"
-#include<clsInputValidate.h>
+#include"clsInputValidate.h"
 
 using namespace std;
+void ReadCleintInfo(clsBankClient &Client)
+{
+
+}
+
+void UpdateClient()
+{
+
+    string AccountNumber = "";
+
+    cout <<"\nPlease Enter Client Account Number: ";
+    AccountNumber =clsInputValidate::ReadString();
+
+    while(!clsBankClient::IsClientExists(AccountNumber))
+    {
+
+        cout <<"\nAccount Number Is Not Found, Choose Another One: ";
+        AccountNumber = clsInputValidate::ReadString();
+
+    }
+
+    clsBankClient Client = clsBankClient::Find(AccountNumber);
+    Client.Print();
+
+    cout << "\nUpdate Client Info: ";
+    cout << "\n------------------------\n";
+
+    ReadCleintInfo(Client);
+
+    clsBankClient ;
+
+}
 
 int main()
 {
     clsBankClient Client1 = clsBankClient::Find("A101");
     Client1.Print();
 
-
+    UpdateClient();
     return 0;
 
 }
