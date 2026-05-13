1029ce529c1145f0ab779195840f62c35a34760f
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-13 20:52:45 +0300

    Edit Files

--- Part 2 ( Applications )/3 - Bank System Project/clsBankClient.h
+++ Part 2 ( Applications )/3 - Bank System Project/clsBankClient.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <clsString.h>
 #include "clsPerson.h"
+#include "clsInputValidate.h"
 
 using namespace std;
 
@@ -261,5 +262,70 @@ public:
         clsBankClient Client1 = clsBankClient::Find(AccountNumber);
 
         return (!Client1.IsEmpty());
+    }
+
+    void ReadCleintInfo(clsBankClient &Client)
+    {
+        cout << "\nEnter FirstName: ";
+        Client.SetFirstName(clsInputValidate::ReadString());
+
+        cout << "\nEnter LastName: ";
+        Client.SetLastName(clsInputValidate::ReadString());
+
+        cout << "\nEnter Email: ";
+        Client.SetEmail(clsInputValidate::ReadString());
+
+        cout << "\nEnter Phone: ";
+        Client.SetPhone(clsInputValidate::ReadString());
+
+        cout << "\nEnter PinCode: ";
+        Client.SetPinCode(clsInputValidate::ReadString());
+
+        cout << "\nEnter Account Balance: ";
+        Client.SetAccountBalance(clsInputValidate::ReadDblNumber());
+    }
+
+    static void UpdateClient()
+    {
+
+        string AccountNumber = "";
+
+        cout << "\nPlease Enter Client Account Number: ";
+        AccountNumber = clsInputValidate::ReadString();
+
+        while (!clsBankClient::IsClientExists(AccountNumber))
+        {
+
+            cout << "\nAccount Number Is Not Found, Choose Another One: ";
+            AccountNumber = clsInputValidate::ReadString();
+        }
+
+        clsBankClient Client = clsBankClient::Find(AccountNumber);
+        Client.Print();
+
+        cout << "\nUpdate Client Info: ";
+        cout << "\n------------------------\n";
+
+        Client.ReadCleintInfo(Client);
+
+        clsBankClient::enSaveResult SaveResult;
+
+        SaveResult = Client.Save();
+
+        switch (SaveResult)
+        {
+        case clsBankClient::enSaveResult::svSucceeded:
+        {
+            cout << "\n\nAccount Updated Successfully :-\n";
+            Client.Print();
+            break;
+        }
+
+        case clsBankClient::enSaveResult::svFaildEmpteObject:
+        {
+            cout << "\nError Account Was Not Saved Because It's Empty.";
+            break;
+        }
+        }
     }
 };
