29b10b566ecb6a1d8d3dcfa439f929ea007b4fc4
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-14 16:06:27 +0300

    Edit 1

--- Part 2 ( Applications )/3 - Bank System Project/Main.cpp
+++ Part 2 ( Applications )/3 - Bank System Project/Main.cpp
@@ -29,6 +29,50 @@ void ReadCleintInfo(clsBankClient &Client)
     Client.SetAccountBalance(clsInputValidate::ReadDblNumber());
 }
 
+void AddNewClient()
+{
+string AccountNumber = "";
+
+        cout << "\nPlease Enter Client Account Number: ";
+        AccountNumber = clsInputValidate::ReadString();
+
+        while (!clsBankClient::IsClientExists(AccountNumber))
+        {
+
+            cout << "\nAccount Number Is Not Found, Choose Another One: ";
+            AccountNumber = clsInputValidate::ReadString();
+        }
+
+        clsBankClient NewClient = clsBankClient::GetAddNewClientObject(AccountNumber);
+
+        ReadCleintInfo(NewClient);
+
+        cout << "\nAdd New Client Info: ";
+        cout << "\n------------------------\n";
+
+        Client.ReadCleintInfo(Client);
+
+        clsBankClient::enSaveResult SaveResult;
+
+        SaveResult = Client.Save();
+
+        switch (SaveResult)
+        {
+        case clsBankClient::enSaveResult::svSucceeded:
+        {
+            cout << "\n\nAccount Added Successfully :-\n";
+            Client.Print();
+            break;
+        }
+
+        case clsBankClient::enSaveResult::svFaildEmpteObject:
+        {
+            cout << "\nError Account Was Not Saved Because It's Empty.";
+            break;
+        }
+        }
+}
+
 int main()
 {
     return 0;
