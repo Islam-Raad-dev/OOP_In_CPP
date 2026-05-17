b29a87df893db1a4eb89c8c128d5e43ba350a0eb
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-17 15:53:37 +0300

    Edit 10

--- Part 2 ( Applications )/3 - Bank System Project/Main.cpp
+++ Part 2 ( Applications )/3 - Bank System Project/Main.cpp
@@ -4,13 +4,12 @@ Bank Project
 
 */
 #include <iostream>   
-#include "clsBankClient.h"
+#include "clsMainScreen.h"
 
 using namespace std;
 
 int main()
 {
-    clsBankClient::ShowTotalBalance();
 
     return 0;
 }
