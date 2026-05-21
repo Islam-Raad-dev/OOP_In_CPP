a177fb93d659fd3741771c9de65c8659b5e00b63
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-21 14:37:37 +0300

    Bank System Project

--- Part 2 ( Applications )/3 - Bank System Project/Main.cpp
+++ Part 2 ( Applications )/3 - Bank System Project/Main.cpp
@@ -10,7 +10,7 @@ banking system. It allows users to log in, view their account details, and
 perform various banking operations such as deposits, withdrawals, and
 transfers. The project is designed to demonstrate the use of object-oriented
 programming concepts in C++.
-
+        
 */
 #include <iostream>   
 #include "clsLoginScreen.h"
