8b50dce767fbd2c2e22ee8417ae4e80aa1831a26
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-24 16:39:36 +0300

    Refactor header comment in Global.h for improved clarity and formatting

--- Part 2 ( Applications )/3 - Bank System Project/Global.h
+++ Part 2 ( Applications )/3 - Bank System Project/Global.h
@@ -1,9 +1,20 @@
+/*
+Core Files:
+
+This is Global Header File in Bank System Project
+
+This file contains global variables and constants that are used throughout 
+the application. It serves as a central location for defining global 
+settings, suchas the current logged-in user, and any other global 
+configurations that may be needed across different modules of the project.
+
+
+*/
+
 #pragma once
+
 #include <iostream>
+
 #include "clsUser.h"
+
 clsUser CurrentUser=clsUser::Find("","");
-
-
-
-
-
