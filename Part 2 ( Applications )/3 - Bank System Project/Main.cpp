287001f29a7ca9bfd8a1b5cd1bceb4cf6784585b
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-24 15:52:39 +0300

    Bank System Project

--- Part 2 ( Applications )/3 - Bank System Project/Main.cpp
+++ Part 2 ( Applications )/3 - Bank System Project/Main.cpp
@@ -8,8 +8,8 @@ Date: 21-5-2026
 Bank System Project is a console-based application that simulates a simple
 banking system. It allows users to log in, view their account details, and
 perform various banking operations such as deposits, withdrawals, and
-transfers. The project is designed to demonstrate the use of object-oriented
-programming concepts in C++.
+transfers. The project is designed to demonstrate the use of
+object-oriented programming concepts in C++.
         
 */
 #include <iostream>   
