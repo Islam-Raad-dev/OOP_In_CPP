1b3c08ef67c17e683b714370eb3d54df44a46c5f
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-24 16:23:57 +0300

    Update header comment for clsLoginScreen class to enhance clarity

--- Part 2 ( Applications )/3 - Bank System Project/clsLoginScreen.h
+++ Part 2 ( Applications )/3 - Bank System Project/clsLoginScreen.h
@@ -1,9 +1,33 @@
+/*
+Screen Files:
+
+This is the login screen of Bank System Project
+
+This file contains the implementation of the login screen of the
+application, which serves as the entry point for users to access the
+banking system. The login screen prompts users to enter their username and 
+password, and it validates the credentials against the stored user data. 
+If the login is successful, the user is granted access to the main screen 
+of the application, where they can navigate to different features and 
+functionalities of the banking system. The login screen also includes a 
+mechanism to handle failed login attempts, allowing users a limited number 
+of tries before locking them out for security reasons. The user interface 
+is designed to be simple and intuitive, providing clear instructions for 
+users to log in and access their accounts. Overall, the login screen is a 
+crucial component of the Bank System Project, ensuring that only 
+authorized users can access the system and providing a secure entry point 
+for users to interact with the application.
+
+*/
+
 #pragma once
 
 #include <iostream>
+#include <iomanip>
+
+
 #include "clsScreen.h"
 #include "clsUser.h"
-#include <iomanip>
 #include "clsMainScreen.h"
 #include "Global.h"
 
