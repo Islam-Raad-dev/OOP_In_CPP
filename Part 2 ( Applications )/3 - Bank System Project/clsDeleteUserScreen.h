67c2ded17b3e890a36e1ef986e117381675dbc0b
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-05-24 16:14:04 +0300

    Update header comment for clsDeleteUserScreen class to enhance clarity

--- Part 2 ( Applications )/3 - Bank System Project/clsDeleteUserScreen.h
+++ Part 2 ( Applications )/3 - Bank System Project/clsDeleteUserScreen.h
@@ -1,3 +1,18 @@
+/*
+Screen Files:
+
+This is Delete User Screen of Bank System Project
+
+This screen is responsible for allowing the user to delete a user from
+the bank system. It prompts the user to enter the username of the
+user they wish to delete, verifies that the username exists, and
+then displays the user's information. The user is then asked to confirm
+if they want to proceed with deleting the user. If confirmed, the user
+is deleted from the system and a success message is displayed. If not
+confirmed, the deletion process is canceled.
+
+*/
+
 #pragma once
 
 #include <iostream>
